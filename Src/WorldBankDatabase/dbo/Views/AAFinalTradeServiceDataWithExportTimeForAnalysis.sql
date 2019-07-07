42af2f8dbb8ffa798ab861ac31e20870a07c648f
Denekew <denekew@hotmail.com>
2019-07-07 10:19:01 -0700

    Added a new view

---
+++ Src/WorldBankDatabase/dbo/Views/AAFinalTradeServiceDataWithExportTimeForAnalysis.sql
@@ -0,0 +1,12 @@
+﻿create view AAFinalTradeServiceDataWithExportTimeForAnalysis
+AS
+select A.*, 
+	B.[GDPGrowthPercent], 
+	C.[LogisticsPerfIndex], 
+	D.[InsuranceAndFinancialSvcPercent], 
+	F.[AverageEportTime]
+FROM [vw_WdImportExportByCountryAndTradeAverageUSD] AS A
+	join [dbo].[vw_WdGdpGrowthPercent] AS B ON A.CountryCode = B.[CountryCode]
+	join [dbo].[vw_WdLogisticsPerformanceIndex] AS C ON A.CountryCode = C.[CountryCode]
+	join [dbo].[vw_WdInsuranceAndFinancialServices] AS D ON A.CountryCode = D.[CountryCode]	
+	join [dbo].[vw_AfAverageEportTime] AS F ON A.CountryCode = F.[CountryCode]
